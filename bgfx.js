5797f86497d0fa40d6bb54683a22dfb3489e9f82
Seo <51868598+Seolavin@users.noreply.github.com>
2022-02-18 22:52:23 -0500

    cleaning up

--- bgfx.js
+++
@@ -1,6 +0,0 @@
-const el = document.querySelector("#bgimg");
-
-el.addEventListener("mousemove", (e) => {
-  el.style.backgroundPositionX = -e.offsetX + "px";
-  el.style.backgroundPositionY = -e.offsetY + "px";
-});
