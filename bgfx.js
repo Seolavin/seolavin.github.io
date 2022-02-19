06d05c6ec7091281da4fce0fdfe8e977241f7e3e
Seo <51868598+Seolavin@users.noreply.github.com>
2022-02-18 22:43:50 -0500

    fixed a few structuring issues, added socials

---
+++ bgfx.js
@@ -0,0 +1,6 @@
+const el = document.querySelector("#bgimg");
+
+el.addEventListener("mousemove", (e) => {
+  el.style.backgroundPositionX = -e.offsetX + "px";
+  el.style.backgroundPositionY = -e.offsetY + "px";
+});
